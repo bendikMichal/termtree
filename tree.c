d5cb3703174f218b089adf44bab73b4548b5b1b9
bendikMichal <mbendik97@gmail.com>
2022-09-07 14:26:53 +0200

    added file sizes

--- tree.c
+++ tree.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <dirent.h>
 #include <sys/stat.h>
+
+void normalize(int bytes) {
+	if (bytes >= pow(10, 12)) {
+		printf("[ %.1f tb ] ", bytes / pow(10, 12));
+	} else if (bytes >= pow(10, 9)) {
+		printf("[ %.1f gb ] ", bytes / pow(10, 9));
+	} else if (bytes >= pow(10, 6)) {
+		printf("[ %.1f mb ] ", bytes / pow(10, 6));
+	} else if (bytes >= pow(10, 3)) {
+		printf("[ %.1f kb ] ", bytes / pow(10, 3));
+	} else if (bytes < pow(10, 3)) {
+		printf("[ %d b ] ", bytes);
+	} 
+}
 
 void ls(char *dirname, DIR *directory, int intedation) {
 	struct dirent *item;
@@ -27,12 +42,14 @@ void ls(char *dirname, DIR *directory, int intedation) {
 				// check if is folder
 				stat(newpath, &st);
 			
+
 				if(S_ISDIR(st.st_mode)) {
 					printf("^ %s\n", item->d_name);
 					// recursive call
 					ls(newpath, directory, intedation + 1);
 				} else {
 					// print name
+					normalize(st.st_size);
 					printf("%s\n", item->d_name);
 				}
 				
